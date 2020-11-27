936d573d7b466a5ef8d089d0e79f6ef66fa31f48
amindal <mohammadaminjafarian1375@gmail.com>
2020-11-27 17:51:32 +0330

    init and test ui service

--- services/uiService.py
+++ services/uiService.py
@@ -1,2 +1,20 @@
+import tkinter
+
+class ui(tkinter.Tk) :
+    def __init__(self,app,title,**karg):
+        tkinter.Tk.__init__(self)
+        self.m_app = app
+        self.title(title)
+        if 'width' in karg.keys() and 'height' in karg.keys() :
+            self.m_width = karg['width']
+            self.m_height = karg['height']
+            if 'x' in karg.keys() and 'y' in karg.keys() :
+                self.m_x = karg['x']
+                self.m_y = karg['y']
+                self.geometry(self.m_width + "x" + self.m_height + "+" + self.m_x + "+" + self.m_y)
+            else :
+                self.geometry(self.m_width + "x" + self.m_height)
+        
+        self.mainloop()
 
 
