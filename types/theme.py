01dbec4a2beaf467212aed42c08ea33906f46b6c
amindal <mohammadaminjafarian1375@gmail.com>
2020-12-03 16:29:51 +0330

    add theme class

---
+++ types/theme.py
@@ -0,0 +1,35 @@
+class theme :
+    #   fields
+    __button_style = {}
+    __label_style = {}
+    __entry_style = {}
+    __window_style = {}
+    #######################
+    
+    #   setters
+    def SetButtonStyle(self, style) : 
+        self.__button_style = style
+    
+    def SetLableStyle(self, style) : 
+        self.__label_style = style
+
+    def SetEntryStyle(self, style) : 
+        self.__entry_style = style
+
+    def SetWindowStyle(self, style) : 
+        self.__window_style = style
+    ###############################################
+
+    #   getters
+    def GetButtonStyle(self) : 
+        return self.__button_style
+
+    def GetLableStyle(self) : 
+        return self.__label_style
+    
+    def GetEntryStyle(self) : 
+        return self.__entry_style
+    
+    def GetWindowStyle(self) : 
+        return self.__window_style
+    ###############################################
