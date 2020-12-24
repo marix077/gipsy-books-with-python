e55b7020d7069f6a766a7ef3419d669dcdb11761
marix077 <mrymjolini@gmail.com>
2020-12-24 06:14:43 -0800

    adding a class call manager

--- app.py
+++ app.py
@@ -2,9 +2,11 @@ from sys import path
 path.append("./services")
 
 from uiService import ui
+from managerService import manager
 
 class app : 
     def __init__(self): 
         self.__ui = ui(self, "gipsy", width="800", height="600", x="100", y="100")
+        self.__manager = manager()
 
 
