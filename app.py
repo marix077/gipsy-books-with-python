1a036b8c163c4e46f2c93f55bb20d73c2fd63611
marix077 <mrymjolini@gmail.com>
2020-12-24 05:57:40 -0800

    remove theme from app

--- app.py
+++ app.py
@@ -1,13 +1,10 @@
 from sys import path
-path.append("./types")
 path.append("./services")
 
 from uiService import ui
-from theme import theme
 
 class app : 
     def __init__(self): 
         self.__ui = ui(self, "gipsy", width="800", height="600", x="100", y="100")
-        self.__theme = theme()
 
 
