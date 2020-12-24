c9ffcef623fd9f26f05b50ffb858d197852c04ab
marix077 <mrymjolini@gmail.com>
2020-12-24 06:30:58 -0800

    create a user class

---
+++ types/user.py
@@ -0,0 +1,15 @@
+class user : 
+    #   field
+    __userName = ""
+    __password = ""
+
+    ###############################
+
+
+    #   constructor
+    def __init__(self, userName, password) : 
+        self.__userName = userName
+        self.__password = password
+
+        
+    #################################
